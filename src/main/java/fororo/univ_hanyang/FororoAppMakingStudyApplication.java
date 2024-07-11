dd7eca741d0e7d6f435f6e802ede0ec655d29f99
poqo <zxvm5962@naver.com>
2024-07-11 21:34:22 +0900

    fix: cors 오류 해결

--- src/main/java/fororo/univ_hanyang/FororoAppMakingStudyApplication.java
+++ src/main/java/fororo/univ_hanyang/FororoAppMakingStudyApplication.java
@@ -2,6 +2,9 @@ package fororo.univ_hanyang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FororoAppMakingStudyApplication {
@@ -9,4 +12,13 @@ public class FororoAppMakingStudyApplication {
 		SpringApplication.run(FororoAppMakingStudyApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://forif.org");
+			}
+		};
+	}
 }
