73f2e13efa7c4499f17942312ea53bc82be1ecec
poqo <zxvm5962@naver.com>
2024-07-12 17:52:12 +0900

    refactor: 변수명 변경

--- src/main/java/fororo/univ_hanyang/study/repository/StudyRepository.java
+++ src/main/java/fororo/univ_hanyang/study/repository/StudyRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 public interface StudyRepository extends JpaRepository<Study,Integer> {
     List<Study> findAllByClubId(Integer clubId);
-    Optional<Study> findByName(String studyName);
+    Optional<Study> findByName(String name);
     Optional<Study> findByMentorIdAndClubId(Integer mentorId, Integer clubId);
     @EntityGraph(attributePaths = {"mentor", "weeklyPlans"})
     @Query("SELECT s FROM Study s WHERE s.clubId = :clubId")
