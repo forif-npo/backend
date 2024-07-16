d928ea764ceeace639736779a36249c500d59864
poqo <zxvm5962@naver.com>
2024-07-16 12:09:55 +0900

    chore: token debug api 엔드포인트 수정

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -7,7 +7,7 @@ APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
 JAR_NAME=$(basename $(ls $DEPLOY_PATH/*.jar))
 
 echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
-nohup java -jar -Dspring.profiles.active=local $DEPLOY_PATH$JAR_NAME --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+nohup java -jar $DEPLOY_PATH$JAR_NAME --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
 sleep 3
 
