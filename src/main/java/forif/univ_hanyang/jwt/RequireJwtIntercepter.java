0f3733774be114fdc558bde7e3ac5a936b91bbf2
poqo <zxvm5962@naver.com>
2024-08-01 17:40:47 +0900

    fix: Header가 없을 때도, 응답이 200으로 가던 오류 수정

--- src/main/java/forif/univ_hanyang/jwt/RequireJwtIntercepter.java
+++ src/main/java/forif/univ_hanyang/jwt/RequireJwtIntercepter.java
@@ -13,7 +13,7 @@ import java.lang.reflect.Method;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class RequireJWTIntercepter implements HandlerInterceptor {
+public class RequireJwtIntercepter implements HandlerInterceptor {
     private final JwtUtils jwtUtils;
 
     @Override
@@ -23,11 +23,15 @@ public class RequireJWTIntercepter implements HandlerInterceptor {
             Method method = handlerMethod.getMethod();
             if (method.isAnnotationPresent(RequireJWT.class) || handlerMethod.getBeanType().isAnnotationPresent(RequireJWT.class)) {
                 String token = request.getHeader("Authorization");
-                // JWT 토큰 검증 로직을 수행하고, 토큰이 유효하지 않다면 예외를 발생시킵니다.
-                if (token == null){
+                if (token == null || !token.startsWith("Bearer ")) {
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authorization token is missing or invalid");
                     return false;
                 }
-                jwtUtils.validateToken(token);
+                token = token.substring(7);
+                if (!jwtUtils.validateToken(token)) {
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid or expired token");
+                    return false;
+                }
             }
         }
         return true;
