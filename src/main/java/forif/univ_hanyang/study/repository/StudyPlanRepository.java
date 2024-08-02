9e3b36ed950ba5c1b9de6af9bfb4eae8c03bb339
poqo <zxvm5962@naver.com>
2024-08-02 17:05:21 +0900

    fix: studyInfo null 관련 오류 해결

--- src/main/java/forif/univ_hanyang/study/repository/StudyPlanRepository.java
+++ src/main/java/forif/univ_hanyang/study/repository/StudyPlanRepository.java
@@ -4,8 +4,9 @@ import forif.univ_hanyang.study.entity.StudyPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudyPlanRepository extends JpaRepository<StudyPlan, Integer> {
     void deleteAllByStudy_Id(Integer studyId);
-    List<StudyPlan> findAllById_StudyIdOrderById_WeekNum(Integer studyId);
+    Optional<List<StudyPlan>> findAllById_StudyIdOrderById_WeekNum(Integer studyId);
 }
