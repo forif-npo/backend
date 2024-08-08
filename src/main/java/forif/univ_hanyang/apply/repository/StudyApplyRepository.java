ec4ad6bb1986a1c4b07d497b9600acd38e6e3126
poqo <zxvm5962@naver.com>
2024-08-08 14:32:50 +0900

    feat: get announcements and get faqs

--- src/main/java/forif/univ_hanyang/apply/repository/StudyApplyRepository.java
+++ src/main/java/forif/univ_hanyang/apply/repository/StudyApplyRepository.java
@@ -1,10 +1,7 @@
 package forif.univ_hanyang.apply.repository;
 
-import forif.univ_hanyang.apply.entity.StudyApply;
+import forif.univ_hanyang.apply.domain.StudyApply;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface StudyApplyRepository extends JpaRepository<StudyApply, Integer> {
 
--- src/main/java/forif/univ_hanyang/user/repository/UserRepository.java
+++ src/main/java/forif/univ_hanyang/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package forif.univ_hanyang.user.repository;
 
-import forif.univ_hanyang.user.entity.User;
+import forif.univ_hanyang.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
