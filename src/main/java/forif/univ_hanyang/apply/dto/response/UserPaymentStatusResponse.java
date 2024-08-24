82c6900315122e8bfc28a26348f69db66a50be32
poqo <zxvm5962@naver.com>
2024-08-24 20:01:20 +0900

    feat: get paid users

--- src/main/java/forif/univ_hanyang/apply/dto/response/UserPaymentStatusResponse.java
+++ src/main/java/forif/univ_hanyang/apply/dto/response/UserPaymentStatusResponse.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-public class UnpaidUserResponse {
+public class UserPaymentStatusResponse {
     private Integer userId;
     private String name;
     private Integer primaryStudyId;
