8b83af5b37b77e697d82a009fca005606dad8ce4
poqo <zxvm5962@naver.com>
2024-08-26 23:37:03 +0900

    refactor: update announcement

--- src/main/java/forif/univ_hanyang/post/dto/request/AnnouncementUpdateRequest.java
+++ src/main/java/forif/univ_hanyang/post/dto/request/AnnouncementUpdateRequest.java
@@ -1,9 +1,12 @@
 package forif.univ_hanyang.post.dto.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class AnnouncementUpdateRequest {
+    private String createdBy;
     private String title;
     private String content;
 }
