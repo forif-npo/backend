d3a3e1061a7662b4931ded350ea21d0aee71cae0
poqo <zxvm5962@naver.com>
2024-08-28 00:52:12 +0900

    refactor: getAllApplications

---
+++ src/main/java/forif/univ_hanyang/apply/dto/response/ApplyInfoResponse.java
@@ -0,0 +1,21 @@
+package forif.univ_hanyang.apply.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApplyInfoResponse {
+    private Integer userId;
+    private String name;
+    private String primaryStudyName;
+    private String secondaryStudyName;
+    private String phoneNumber;
+    private String intro;
+    private String applyPath;
+    private String payYn;
+    private String primaryStatus;
+    private String secondaryStatus;
+}
