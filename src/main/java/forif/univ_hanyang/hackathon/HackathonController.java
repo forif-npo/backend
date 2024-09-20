fb870f86ecdc9220f9818ba980671f7cc43d3128
poqo <zxvm5962@naver.com>
2024-09-20 15:02:42 +0900

    docs: 해커톤 추가

--- src/main/java/forif/univ_hanyang/hackathon/HackathonController.java
+++ src/main/java/forif/univ_hanyang/hackathon/HackathonController.java
@@ -1,5 +1,8 @@
 package forif.univ_hanyang.hackathon;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "해커톤", description = "해커톤 관련 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/hackathons")
 public class HackathonController {
     private final HackathonService hackathonService;
 
+    @Operation(
+            summary = "전체 해커톤 조회",
+            description = "전체 해커톤을 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK")
+            }
+    )
     @GetMapping
     public ResponseEntity<List<HackathonResponse>> getHackathons() {
         return new ResponseEntity<>(hackathonService.getHackathons(), HttpStatus.OK);
