b719029a63f2d6bb1e5085d4c71e2b2934156655
poqo <zxvm5962@naver.com>
2024-09-24 15:38:28 +0900

    refactor: sendAlimtalk

--- src/main/java/forif/univ_hanyang/alimtalk/AlimTalkRequest.kt
+++ src/main/java/forif/univ_hanyang/alimtalk/AlimTalkRequest.kt
@@ -3,7 +3,6 @@ package forif.univ_hanyang.alimtalk
 data class AlimTalkRequest (
     val receivers: List<String>,
     val templateCode: String,
-    val studyName: String?,
     val responseSchedule: String?,
     val dateTime: String?,
     val location: String?,
